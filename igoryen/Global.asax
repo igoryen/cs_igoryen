bdcc7bb559e215506b9b0b3aa5679b554239209e
igoryen <igor.yentaltsev@gmail.com>
2014-03-20 21:21:41 -0400

    first commit

---
+++ igoryen/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="igoryen.MvcApplication" Language="C#" %>
---
+++ igoryen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(igoryen.Startup))]
+namespace igoryen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
