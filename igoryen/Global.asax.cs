b88b3aaff75b1180bc814635ba1c346076fa93be
igoryen <igor.yentaltsev@gmail.com>
2014-03-23 19:06:12 -0400

    Global.asax: added AutoMapper maps

--- igoryen/Global.asax.cs
+++ igoryen/Global.asax.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using igoryen.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
 
 namespace igoryen
 {
@@ -16,6 +18,38 @@ namespace igoryen
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            System.Data.Entity.Database.SetInitializer(new igoryen.Models.Initializer());
+
+            // to ViewModels/classes
+
+            Mapper.CreateMap<Models.Course, ViewModels.CourseBase>();
+            Mapper.CreateMap<Models.Course, ViewModels.CourseFull>();
+
+            Mapper.CreateMap<Models.Faculty, ViewModels.FacultyBase>();
+            Mapper.CreateMap<Models.Faculty, ViewModels.FacultyFull>();
+
+            Mapper.CreateMap<Models.Message, ViewModels.MessageBase>();
+            Mapper.CreateMap<Models.Message, ViewModels.MessageFull>();
+
+            Mapper.CreateMap<Models.Student, ViewModels.StudentBase>();
+            Mapper.CreateMap<Models.Student, ViewModels.StudentFull>();
+
+
+            // from ViewModels/classes
+
+            Mapper.CreateMap<ViewModels.CourseBase, Models.Course>();
+            Mapper.CreateMap<ViewModels.CourseFull, Models.Course>();
+
+            Mapper.CreateMap<ViewModels.FacultyBase, Models.Faculty>();
+            Mapper.CreateMap<ViewModels.FacultyFull, Models.Faculty>();
+
+            Mapper.CreateMap<ViewModels.MessageBase, Models.Message>();
+            Mapper.CreateMap<ViewModels.MessageFull, Models.Message>();
+
+            Mapper.CreateMap<ViewModels.StudentBase, Models.Student>();
+            Mapper.CreateMap<ViewModels.StudentFull, Models.Student>();
+
         }
     }
 }
