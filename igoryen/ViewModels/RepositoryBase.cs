bdbe416a45d8905c0de1981cdf08bf3159264e3e
igoryen <igor.yentaltsev@gmail.com>
2014-03-23 11:39:32 -0400

    ViewModels/RepositoryBase.cs: added

---
+++ igoryen/ViewModels/RepositoryBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using igoryen.ViewModels;
+using igoryen.Models;
+
+namespace igoryen.ViewModels {
+  public class RepositoryBase {
+
+    protected DataContext dc;
+
+    //====================================================================
+    // the class's constructor
+    // 20. 20. Turn off the Entity Framework (EF) proxy creation features
+    //   We do NOT want the EF to track changes - we'll do that ourselves
+    // 25. Also, turn off lazy loading...
+    //   We want to retain control over fetching related objects
+    //====================================================================
+    public RepositoryBase() {
+
+      dc = new DataContext();
+
+      dc.Configuration.ProxyCreationEnabled = false; // 20
+      dc.Configuration.LazyLoadingEnabled = false; // 25
+    }
+
+  }
+}
