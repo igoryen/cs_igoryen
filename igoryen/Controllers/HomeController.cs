794883924525fb4c954bd6890ec56731813638ee
igoryen <igor.yentaltsev@gmail.com>
2014-03-23 20:31:34 -0400

    Controllers/HomeController.cs/Index(): modified

--- igoryen/Controllers/HomeController.cs
+++ igoryen/Controllers/HomeController.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿// 10. defines UserManager, RoleManager
+// 20. identity role
+// 45. for HttpStatusCode
+// 50. for Task<>
+using igoryen.Models;
+using igoryen.ViewModels;
+using Microsoft.AspNet.Identity; // 10
+using Microsoft.AspNet.Identity.EntityFramework; // 20
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,8 +14,17 @@ using System.Web.Mvc;
 
 namespace igoryen.Controllers {
   public class HomeController : Controller {
+
+    //==================================================
+    // Bring in namespaces
+    //==================================================
+    private DataContext db = new DataContext();
+    private UserManager<ApplicationUser> manager;
+    private Repo_Faculty rf = new Repo_Faculty();
+
     public ActionResult Index() {
-      return View();
+      //return View();
+      return View(rf.getListOfFacultyBase());
     }
 
     public ActionResult About() {
