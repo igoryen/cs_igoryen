4f15662c34ef612e30bb43f8c5b1977ea9b52f1d
igoryen <igor.yentaltsev@gmail.com>
2014-04-17 19:02:54 -0400

    ViewModels/VM_Cancellation.cs: formatted

--- igoryen/ViewModels/VM_Cancellation.cs
+++ igoryen/ViewModels/VM_Cancellation.cs
@@ -6,42 +6,42 @@ using System.ComponentModel.DataAnnotations;
 
 namespace igoryen.ViewModels {
 
-  // Classes alphabetically
+    // Classes alphabetically
 
-  //======================================
-  // CancellationBase
-  //======================================
-  public class CancellationBase {
+    //======================================
+    // CancellationBase
+    //======================================
+    public class CancellationBase {
 
-    [Key]
-    public int CancellationId { get; set; }
-    public string Date { get; set; }
-  }
-
-  //======================================
-  // CancellationCreate - 4 fields/forms
-  //======================================
-  public class CancellationCreate {
-    [Key]
-    public int CancellationId { get; set; }
-    public FacultyBase Faculty { get; set; }
-    [Required]
-    public string CourseCode { get; set; }
-    public string Date { get; set; }
-  }
-
-
-  //======================================
-  // CancellationFull
-  //======================================
-  public class CancellationFull : CancellationBase {
-    public string CourseName { get; set; }
-    public FacultyFull Faculty { get; set; }
-    public CourseBase Course { get; set; }
-
-    public CancellationFull() {
-      this.Faculty = new FacultyFull();
-      this.Course = new CourseBase();
+        [Key]
+        public int CancellationId { get; set; }
+        public string Date { get; set; }
     }
-  }
+
+    //======================================
+    // CancellationCreate - 4 fields/forms
+    //======================================
+    public class CancellationCreate {
+        [Key]
+        public int CancellationId { get; set; }
+        public FacultyBase Faculty { get; set; }
+        [Required]
+        public string CourseCode { get; set; }
+        public string Date { get; set; }
+    }
+
+
+    //======================================
+    // CancellationFull
+    //======================================
+    public class CancellationFull : CancellationBase {
+        public string CourseName { get; set; }
+        public FacultyFull Faculty { get; set; }
+        public CourseBase Course { get; set; }
+
+        public CancellationFull() {
+            this.Faculty = new FacultyFull();
+            this.Course = new CourseBase();
+        }
+    }
 }
